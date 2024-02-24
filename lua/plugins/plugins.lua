60846ac74f2993c2f91ffe8b0ae5a635efb525b6
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2024-02-24 17:22:58 +0300

    Add tailwind sorter plugin

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -41,6 +41,16 @@ return {
       },
     },
   },
+  {
+    "laytan/tailwind-sorter.nvim",
+    -- lazy = true,
+    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-lua/plenary.nvim" },
+    build = "cd formatter && npm i && npm run build",
+    config = true,
+    -- opts = {
+    --   on_save_enabled = true,
+    -- },
+  },
   -- {
   --   "nvim-telescope/telescope-ui-select.nvim",
   -- },
