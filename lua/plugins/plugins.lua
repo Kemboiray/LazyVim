5404e857b31f0a7cd942d482c642adc373f90330
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2024-02-25 11:18:26 +0300

    Create branch for windows

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -44,12 +44,28 @@ return {
   {
     "laytan/tailwind-sorter.nvim",
     -- lazy = true,
+    enabled = false,
     dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-lua/plenary.nvim" },
     build = "cd formatter && npm i && npm run build",
     config = true,
     -- opts = {
     --   on_save_enabled = true,
     -- },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function ()
+      require 'nvim-treesitter.install'.prefer_git = true
+      require 'nvim-treesitter.install'.compilers = { "clang", "gcc" }
+      local configs = require("nvim-treesitter.configs")
+      configs.setup({
+          ensure_installed = { "c", "lua", "vim", "vimdoc", "python", "javascript", "html" },
+          sync_install = false,
+          highlight = { enable = true },
+          indent = { enable = true },  
+        })
+    end
   },
   -- {
   --   "nvim-telescope/telescope-ui-select.nvim",
